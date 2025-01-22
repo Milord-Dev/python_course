170d9aa36f240555bda4b43850a64d27f6df23e0
ricardo milord <milord.dev05@gmail.com>
2025-01-22 11:04:37 -0500

    PrimerosProgramas

---
+++ eu_libra.py
@@ -0,0 +1,9 @@
+#De Libras a euros. Diseña un programa realice el cambio de libras a euros.valor de la libra
+
+valorMonetario = float(input("Ingrese su valor en libras: "))
+print("Dato: El cambio esta a: 1.2 euros")
+valorConver = float(input("Ingresar el valor de la conversion: "))
+
+formulaConver = (valorMonetario*valorConver)
+
+print(f"Su conversion total en euros es de: {formulaConver}")
