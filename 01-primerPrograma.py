94c639f9f8944fec3b9320e83a2535657b8df119
ricardo milord <milord.dev05@gmail.com>
2026-04-29 00:42:56 -0500

    Add program to find maximum and minimum of three numbers

---
+++ 01-primerPrograma.py
@@ -0,0 +1,6 @@
+num1 = int(input("Ingrese el numero 1: "))
+num2 = int(input("Ingrese el numero 2: "))
+num3 = int(input("Ingrese el numero 3: "))
+
+print(f"El numero maximo es: ", {max(num1,num2,num3)})
+print(f"El numero minimo es: ", {min(num1,num2,num3)})
