ba8173d25e4a1484abf84455a0cf13cf3dfa235a
ricardo milord <milord.dev05@gmail.com>
2026-04-29 01:05:07 -0500

    Refactor input handling and enhance comments for clarity

--- 01-primerPrograma.py
+++ 01-primerPrograma.py
@@ -1,6 +1,21 @@
+#Version inicial del curso
 num1 = int(input("Ingrese el numero 1: "))
 num2 = int(input("Ingrese el numero 2: "))
 num3 = int(input("Ingrese el numero 3: "))
 
 print(f"El numero maximo es: ", {max(num1,num2,num3)})
 print(f"El numero minimo es: ", {min(num1,num2,num3)})
+
+# Version mas Pro
+# def pedir_valor(cantidad):
+#     numeros = []
+
+#     for i in range(cantidad):
+#         numero = int(input(f"Ingrese los numero {i+1}: "))
+#         numeros.append(numero)
+#     return numeros
+
+# valor = pedir_valor(5)
+# print(max(valor))
+# print(min(valor))
+
