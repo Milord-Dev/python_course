a36a47bed40e8ec4e8c155f19c316b7f795a56a7
ricardo milord <milord.dev05@gmail.com>
2026-06-23 23:53:35 -0500

    Initialize inventory structure and add placeholder functions for inventory management

--- 06-inventario.py
+++ 06-inventario.py
@@ -1,5 +1,9 @@
 #Inventario de tienda 
 
+inventario = {
+    
+}
+
 #Clase producto
 class producto:
     def __init__(self,id, nombre, precio, cantidad, categoria):
@@ -9,6 +13,19 @@ class producto:
         self.cantidad = cantidad
         self.categoria = categoria
 
-class inventario:
-    def __init__(self):
-        self.productos = {}
+#Esoera que el usuario agregue un nuevo producto o el admin 
+#Funciones para inventario
+def agregar_productor(respuesta_usuario):
+    pass
+
+def buscar_producto():
+    pass
+
+def mostrar_producto():
+    pass
+
+def calcular_valor_inventario():
+    pass
+
+def notificacion_producto():
+    pass
