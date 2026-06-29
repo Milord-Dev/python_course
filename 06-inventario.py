62ce3161419f49390329f256abd84ee7146a38e1
ricardo milord <milord.dev05@gmail.com>
2026-06-29 00:22:55 -0500

    Refactor product and inventory classes for consistency and clarity

--- 06-inventario.py
+++ 06-inventario.py
@@ -1,31 +1,48 @@
-#Inventario de tienda 
-
-inventario = {
-    
-}
-
 #Clase producto
-class producto:
+class Producto:
     def __init__(self,id, nombre, precio, cantidad, categoria):
         self.id = id
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
         self.categoria = categoria
+    
+    #Funciones del producto
+    def estado_producto():
+        pass
 
-#Esoera que el usuario agregue un nuevo producto o el admin 
-#Funciones para inventario
-def agregar_productor(respuesta_usuario):
-    pass
+    def reponer_stock():
+        pass
 
-def buscar_producto():
-    pass
+    def calcular_precio_producto():
+        pass
 
-def mostrar_producto():
-    pass
+#Clase Inventario
+class Inventario:
+    def __init__(self):
+        self.dict_inventario = {}
 
-def calcular_valor_inventario():
-    pass
+    #Funciones para inventario
+    def agregar_producto():
+        pass
 
-def notificacion_producto():
-    pass
+    def buscar_producto():
+        pass
+
+    def mostrar_producto():
+        pass
+
+    def calcular_valor_inventario():
+        pass
+
+    def notificacion_producto():
+        pass
+
+print("--- Bienvenido a la tiendo basai fantasia --- \n")
+print("Elija una de las opciones: \n")
+interfaz = input("""
+    MENU PRINCIPAL:
+    1. Admin
+    2. Usuario
+    3. Salir
+""")
